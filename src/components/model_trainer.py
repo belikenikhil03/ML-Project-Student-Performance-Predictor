5a7b48d7ea5bdeb5f883d49c5fd1b00785855c7a
belikenikhil03 <nikhilarora0309@gmail.com>
2024-07-16 17:02:15 +0530

    Hyperparameter Tuning done

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -47,23 +47,22 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
             params={
-                "Decision Tree": {
+                "Decision Tree":{
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    # 'splitter':['best','random'],
-                    # 'max_features':['sqrt','log2'],
+                    'splitter':['random','best'],
+                    'max_features':['sqrt', 'log2']
                 },
                 "Random Forest":{
-                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                 
-                    # 'max_features':['sqrt','log2',None],
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],                
+                    'max_features':['sqrt','log2',None],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Gradient Boosting":{
-                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
                     'learning_rate':[.1,.01,.05,.001],
                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    # 'criterion':['squared_error', 'friedman_mse'],
-                    # 'max_features':['auto','sqrt','log2'],
+                    'criterion':['squared_error', 'friedman_mse'],
+                    'max_features':['auto','sqrt','log2'],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
@@ -78,7 +77,7 @@ class ModelTrainer:
                 },
                 "AdaBoost Regressor":{
                     'learning_rate':[.1,.01,0.5,.001],
-                    # 'loss':['linear','square','exponential'],
+                    'loss':['linear','square','exponential'],
                     'n_estimators': [8,16,32,64,128,256]
                 }
                 
@@ -87,10 +86,9 @@ class ModelTrainer:
             model_report:dict=evaluate_model(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
                                              models=models,param=params)
             
-            ## To get best model score from dict
+            
             best_model_score = max(sorted(model_report.values()))
 
-            ## To get best model name from dict
 
             best_model_name = list(model_report.keys())[
                 list(model_report.values()).index(best_model_score)
